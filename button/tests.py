13583aaa6658a3c15ef8e2ec6e84493b6d9ed9f5
Grant MacGillivray <grant@snuggle.cat>
2018-11-17 18:00:42 -0800

    turns out localhost wasn't working, so trying something else

--- button/tests.py
+++ button/tests.py
@@ -1,16 +1,17 @@
 from django.test import TestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 # Create your tests here.
-class ButtonTest(TestCase):
+class ButtonTest(StaticLiveServerTestCase):
 
 	def setUp(self):
 		self.driver = webdriver.Chrome()
 		
 	def test_button(self):
 		driver = self.driver
-		driver.get("http://localhost:8000/button/")
+		driver.get("%s%s" % (self.live_server_url, "/button/"))
 		print(driver.page_source.encode("utf-8"))
 		driver.find_element_by_id("greeting").click()
 		elem = driver.find_element_by_id("showit")
