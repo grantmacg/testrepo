f4255689edc863d836c44001068e20958b16866d
Grant MacGillivray <grant@snuggle.cat>
2018-11-17 17:25:43 -0800

    adding selenium tests

--- button/tests.py
+++ button/tests.py
@@ -1,8 +1,19 @@
 from django.test import TestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 # Create your tests here.
 class ButtonTest(TestCase):
 
+	def setUp(self):
+        self.driver = webdriver.Firefox()
+		
 	def test_button(self):
-		response = self.client.get('/button/')
-		self.assertEqual(response.status_code, 200)
+		driver = self.driver
+		driver.get("http://localhost:8000/button/)
+		driver.find_element_by_id("greeting").click()
+		elem = driver.find_element_by_id("showit")
+		assert "...world!" in elem
+		
+	def tearDown(self):
+		self.driver.close()
