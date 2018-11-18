83bbd40542b0724ea56858f21cc8452ebf2f14b5
Grant MacGillivray <grant@snuggle.cat>
2018-11-17 16:39:06 -0800

    after all that, forgot to make the test a subclass of TestCase

--- button/tests.py
+++ button/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 
 # Create your tests here.
-def test_button(self):
-	response = self.client.get('/button/')
-	self.assertEqual(response.status_code, 200)
+class ButtonTest(TestCase):
+
+	def test_button(self):
+		response = self.client.get('/button/')
+		self.assertEqual(response.status_code, 200)
